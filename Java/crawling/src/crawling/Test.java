bee80593dbbeb3cc3d4ba192c5c8c36f77d70bf7
jso4342 <jso4342@naver.com>
2022-08-19 19:20:54 +0900

    CGV 영화 데이터 크롤링

---
+++ Java/crawling/src/crawling/Test.java
@@ -0,0 +1,9 @@
+package crawling;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("(20)".substring("(20)".indexOf("(") + 1, "(20)".indexOf(")")));
+		
+		
+	}
+}
