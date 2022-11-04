f1e2618fae8ddd317365791422d9566b7781bbe9
jso4342 <jso4342@naver.com>
2022-11-04 23:19:48 +0900

    demo study 완료

--- Java/demo/app/src/main/java/com/codesoom/demo/DemoHttpHandler.java
+++ Java/demo/app/src/main/java/com/codesoom/demo/DemoHttpHandler.java
@@ -1,17 +1,19 @@
 package com.codesoom.demo;
 
 import com.codesoom.demo.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DemoHttpHandler implements HttpHandler {
+    private ObjectMapper objectMapper = new ObjectMapper();
     private List<Task> tasks = new ArrayList<>();
 
     public DemoHttpHandler() {
@@ -19,6 +21,11 @@ public class DemoHttpHandler implements HttpHandler {
         task.setId(1L);
         task.setTitle("Do Nothing...");
         tasks.add(task);
+
+        Task task2 = new Task();
+        task2.setId(2L);
+        task2.setTitle("Second");
+        tasks.add(task2);
     }
 
     @Override
@@ -32,11 +39,23 @@ public class DemoHttpHandler implements HttpHandler {
         URI uri = exchange.getRequestURI();
         String path = uri.getPath();
 
-        System.out.println(method + " " + path);
+        // 바디 읽어오기
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+            .lines()
+            .collect(Collectors.joining("\n"));
 
+        System.out.println(method + " " + path);
+        if (!body.isBlank()){
+           // System.out.println(body);
+            Task task = toTask(body);
+            tasks.add(task);
+          //  System.out.println(task);
+        }
         String content = "Hello World!";
 
         if (method.equals("GET") && path.equals("/tasks")){
+            content = tasksToJSON();
          //   content = tasks.toString();
            // content = "[{\"id\":" + tasks.get(0).getId() + ", \"title\":" + tasks.get(0).getTitle()+ "}]"; 이렇게 할 수는 있지만 너무 노가다...
         }
@@ -51,5 +70,16 @@ public class DemoHttpHandler implements HttpHandler {
         outputStream.write(content.getBytes()); // write 가 byte[] 를 매개변수로 받음
         outputStream.flush();
         outputStream.close();
+    }
+
+    private Task toTask(String content) throws JsonProcessingException {
+        return objectMapper.readValue(content, Task.class);
+    }
+
+    private String tasksToJSON() throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+
+        return outputStream.toString();
     }
 }
